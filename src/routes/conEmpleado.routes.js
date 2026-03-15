c1a674e09e4175d2f94ebd631bef8656772aa411
Genesis <genesisvaldivia696@gmail.com>
2026-03-15 08:07:56 -0600

    tt

--- src/routes/conEmpleado.routes.js
+++ src/routes/conEmpleado.routes.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import * as ctrl from '../controllers/conEmpleados.controladores.js';
-import { authenticateToken } from '../middleware/auth.js';  
 
 const router = Router();
 
