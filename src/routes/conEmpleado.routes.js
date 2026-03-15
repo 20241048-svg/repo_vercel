3807a0ddccb9ad087ac2a6894b6f79dabd5f6bad
Genesis <genesisvaldivia696@gmail.com>
2026-03-15 12:44:30 -0600

    veri

--- src/routes/conEmpleado.routes.js
+++ src/routes/conEmpleado.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import * as ctrl from '../controllers/conEmpleados.controladores.js';
+import { verificarToken } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 //si
 // Ruta protegida (solo usuarios autenticados)
-router.get('/empleado/:idEmpleado', ctrl.reporteEmpleado);
+router.get('/empleado/:idEmpleado', verificarToken,ctrl.reporteEmpleado);
 
 export default router;
 
