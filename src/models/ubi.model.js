80ebc7b43655dfdd5c6b99180e2ebd0bfe6b0d86
Genesis <genesisvaldivia696@gmail.com>
2026-03-19 21:29:36 -0600

    cor

--- src/models/ubi.model.js
+++ src/models/ubi.model.js
@@ -3,7 +3,7 @@ import db from '../config/db.js'
 export const obtenerUbicaciones = async () => {
 
   const [rows] = await db.query(
-    `SELECT id, descripcion, imagen_nombre, url
+    `SELECT id, descripcion, imagen_nombre, url, latitud, longitud
      FROM ubicaciones`
   );
 //no se
