e41bc8a15bf91666fb75b06d29ab570fb5781519
Genesis <genesisvaldivia696@gmail.com>
2026-03-21 23:12:22 -0600

    veri

--- src/routes/contacto.routes.js
+++ src/routes/contacto.routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import * as ctrl from '../controllers/contacto.controladores.js';
-//import { verificarToken } from '../middlewares/auth.middleware.js';
+import { verificarToken } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 //si
 // Ruta protegida (solo usuarios autenticados)
-router.get('/' ,ctrl.getContactoController);
-router.put('/', ctrl.updateContactoController);
+router.get('/' ,verificarToken, ctrl.getContactoController);
+router.put('/', verificarToken, ctrl.updateContactoController);
 
 export default router;
 
